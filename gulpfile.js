a3e5c26cd3356de41e3bbcfa4328a561af5e9503
Ilia Zhitenev <iyzhitenev@gmail.com>
2026-03-25 20:05:53 +0000

    fix problems with building

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,29 @@ const template = require('gulp-template').default;
 const rename = require('gulp-rename');
 const fs = require('fs');
 
-async function compileHome() {
+function compileHome() {
     try {
         const dataPath = './data.json';
         if (!fs.existsSync(dataPath)) {
             console.error('Ошибка: Файл data.json не найден!');
-            return;
+            return Promise.resolve();
         }
 
         const personalData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 
         return gulp.src('./index.template.hbs')
             .pipe(template(personalData))
+            .on('error', function (err) {
+                console.error('--- Ошибка шаблонизатора! ---');
+                console.error(err.message);
+                this.emit('end'); // Не даем процессу зависнуть
+            })
             .pipe(rename('index.hbs'))
-            .pipe(gulp.dest('./'));
+            .pipe(gulp.dest('./'))
+            .on('end', () => console.log('--- index.hbs успешно создан ---'));
     } catch (err) {
         console.error('Ошибка парсинга JSON или чтения файла:', err.message);
+        return Promise.resolve();
     }
 }
 
